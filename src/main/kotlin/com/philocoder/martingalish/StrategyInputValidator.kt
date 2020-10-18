99508458bba401e5dad6ff30265751073b4df61c
DMARKET\melifoglu <mert.elifoglu@hepsiburada.com>
2020-10-18 20:46:59 +0300

    add char validation

--- src/main/kotlin/com/philocoder/martingalish/StrategyInputValidator.kt
+++ src/main/kotlin/com/philocoder/martingalish/StrategyInputValidator.kt
@@ -1,15 +1,20 @@
 package com.philocoder.martingalish
 
-import com.philocoder.martingalish.DesiredBetResult.GainMoney
+import com.philocoder.martingalish.DesiredBetResult.*
 
 object StrategyInputValidator {
+
+    private val allValidChars = arrayOf(GainMoney, LoseMoney, BackToBankroll).map { it.representation }
 
     fun isValid(input: String): Boolean {
         if (!input.startsWith(GainMoney.representation)) {
             println("Your strategy should start with a ${GainMoney.representation}")
             return false
         }
-        //input.toCharArray().all { }
+        if (input.toCharArray().any { !allValidChars.contains(it) }) {
+            println("Your strategy contains invalid chars. Valid chars are: $allValidChars")
+            return false
+        }
         return true
     }
 }
